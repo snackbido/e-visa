322adbe0f63cb86f6551b73cd2b245691023db72
snackbido <hatran12387@gmail.com>
2025-09-26 14:52:13 +0700

    add auth guard for payment route

--- src/payment/payment.controller.ts
+++ src/payment/payment.controller.ts
@@ -1,16 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { Body, Controller, Get, Ip, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Ip,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { PaymentService } from '@visa/payment/payment.service';
 import { PaymentDto } from '@visa/payment/dto/payment.dto';
 import { Payment } from '@visa/payment/entity/payment.entity';
+import { JwtAuthGuard } from '@visa/auth/auth.guard';
+import { RolesGuard } from '@visa/auth/role.guard';
+import { Roles } from '@visa/auth/roles.decorator';
+import { ROLES } from '@visa/user/entity/user.entity';
 
 @Controller('/api/payment')
+@UseGuards(JwtAuthGuard)
 export class PaymentController {
   constructor(private paymentService: PaymentService) {}
 
   @Get()
+  @UseGuards(RolesGuard)
+  @Roles([ROLES.ADMIN])
   async getAll(): Promise<Payment[]> {
     return await this.paymentService.getPayments();
   }
@@ -29,7 +44,7 @@ export class PaymentController {
   }
 
   @Post('ipn')
-  handleIpn(@Body() body, @Query() query) {
+  handleIpn(@Body() body: any, @Query() query: any) {
     const params = { ...query, ...body };
 
     return this.paymentService.handleIpn(params);
