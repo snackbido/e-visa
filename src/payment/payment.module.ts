9955a0f8244beec95074618a7c7808c5c622b922
snackbido <hatran12387@gmail.com>
2025-09-27 08:44:08 +0700

    add cached service in payment

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -7,9 +7,11 @@ import { OnePayService } from '@visa/utils/onepay/onepay.service';
 import { OnePayModule } from '@visa/utils/onepay/onepay.module';
 import { UserRepository } from '@visa/repository/user.repository';
 import { EmailService } from '@visa/utils/email/email.service';
+import { RedisCachedModule } from '@visa/utils/cached/redis.module';
+import { RedisService } from '@visa/utils/cached/redis.service';
 
 @Module({
-  imports: [DatabaseConfigModule, OnePayModule],
+  imports: [DatabaseConfigModule, OnePayModule, RedisCachedModule],
   controllers: [PaymentController],
   providers: [
     PaymentService,
@@ -17,6 +19,7 @@ import { EmailService } from '@visa/utils/email/email.service';
     OnePayService,
     UserRepository,
     EmailService,
+    RedisService,
   ],
 })
 export class PaymentModule {}
