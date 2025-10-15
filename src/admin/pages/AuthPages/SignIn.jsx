611acad8a3f6c3459ece74f6e018f1cf7fa87f6a
snackbido <hatran12387@gmail.com>
2025-10-15 09:23:42 +0700

    fix some authentication logic

--- src/admin/pages/AuthPages/SignIn.jsx
+++ src/admin/pages/AuthPages/SignIn.jsx
@@ -2,12 +2,12 @@ import PageMeta from "../../components/common/PageMeta";
 import AuthLayout from "./AuthPageLayout";
 import SignInForm from "../../components/auth/SignInForm";
 
-export default function SignIn() {
+export default function SignIn({ currentUser }) {
   return (
     <>
       <PageMeta title="" description="" />
       <AuthLayout>
-        <SignInForm />
+        <SignInForm currentUser={currentUser} />
       </AuthLayout>
     </>
   );
--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 
 export const AdminLayout = ({ currentUser }) => {
-  console.log(currentUser);
   if (currentUser?.role === "admin") {
     return <Outlet />;
   } else {
+    localStorage.removeItem("user")
+    localStorage.removeItem("Authorization")
     return <Outlet />;
   }
 };
