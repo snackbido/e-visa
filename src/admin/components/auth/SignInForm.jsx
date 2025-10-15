78e5cc43967c9235965b75c75d1867fbf3562fd5
snackbido <hatran12387@gmail.com>
2025-10-15 09:35:03 +0700

    fix some authentication logic

--- src/admin/components/auth/SignInForm.jsx
+++ src/admin/components/auth/SignInForm.jsx
@@ -25,9 +25,6 @@ export default function SignInForm({ currentUser }) {
   );
 
   useEffect(() => {
-    if (!user) {
-      setIsLoading(false);
-    }
     if (message && !user) {
       toast.error(message);
     }
